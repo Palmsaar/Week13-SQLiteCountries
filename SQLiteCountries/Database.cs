c72ee50f6467b3303a600e12006efb73d4621a21
Aron Palmsaar <apalmsaar@gmail.com>
2022-02-21 10:59:43 +0200

    Add project files.

---
+++ SQLiteCountries/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Database
+    {
+        public SQLiteConnection myConnetion;
+
+        public Database()
+        {
+            myConnetion = new SQLiteConnection("Data Source=Countries.db");
+
+        }
+
+        public void OpenConnection()
+        {
+            if(myConnetion.State != System.Data.ConnectionState.Open)
+            {
+                myConnetion.Open();
+            }
+        }
+        public void CloseConnection()
+        {
+            if (myConnetion.State != System.Data.ConnectionState.Closed)
+            {
+                myConnetion.Close();
+            }
+        }
+    }
+}
---
+++ SQLiteCountries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCountries();
+        }
+
+        private static void ReadCountries()
+        {
+            Database databaseObj = new Database();
+            string query = "SELECT Country.CountryName as Country, Capital.capitalname as Capital FROM Capital JOIN Country on Capital.countryId = Country.rowid";
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObj.myConnetion);
+            databaseObj.OpenConnection();
+
+            SQLiteDataReader data = myCommand.ExecuteReader();
+
+            if (data.HasRows)
+            {
+                while (data.Read())
+                {
+                    Console.WriteLine($"Country: {data["Country"]}, Capital: {data["Capital"]}");
+                }
+            }
+
+            databaseObj.CloseConnection();
+        }
+
+    }
+}
